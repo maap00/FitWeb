705bfca9e092de7ef053fd2f9246f84ff86dc132
maap00 <marco.astudillo91@gmail.com>
2022-11-01 08:26:16 -0300

    Added Scroll

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,36 @@
+(function(){
 
-var propSlider = {
-    slider: document.getElementById('slider'),
-    firstSlider: null
-}
-
-var metSlider = {
-
-    init: function(){
-        setInterval(metSlider.moveSlider,3000)
-    },
-
-
-    moveSlider: function(){
-        propSlider.slider.style.transition = 'all 1s ease';
-        propSlider.slider.style.marginLeft = '-100%';
-
-        setTimeout(function(){
-            propSlider.firstSlider= propSlider.slider.firstElementChild;
-            propSlider.slider.appendChild(propSlider.firstSlider);
-            propSlider.slider.style.transition = 'unset';
-            propSlider.slider.style.marginLeft = 0;
-
-        },1000)
+    var propSlider = {
+        slider: document.getElementById('slider'),
+        firstSlider: null
     }
+    
+    var metSlider = {
+    
+        init: function(){
+            setInterval(metSlider.moveSlider,3000)
+        },
+    
+    
+        moveSlider: function(){
+            propSlider.slider.style.transition = 'all 1s ease';
+            propSlider.slider.style.marginLeft = '-100%';
+    
+            setTimeout(function(){
+                propSlider.firstSlider= propSlider.slider.firstElementChild;
+                propSlider.slider.appendChild(propSlider.firstSlider);
+                propSlider.slider.style.transition = 'unset';
+                propSlider.slider.style.marginLeft = 0;
+    
+            },1000)
+        }
+    
+    }
+    
+    metSlider.init();
+    
+    
+    
+    
 
-}
-
-metSlider.init();
-
-
-
+}())
